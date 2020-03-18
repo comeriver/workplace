2aa0d52f3b2471075256750794ad6f706e645f80
aycom366 <bamigboyeayomide200@gmail.com>
2020-03-18 08:23:53 +0100

    files neccessary to make the app work added already

---
+++ advanced_ckey/getwebpage.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace advanced_ckey
+{
+    partial class getwebpage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtwebclient = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtwebclient
+            // 
+            this.txtwebclient.Location = new System.Drawing.Point(140, 331);
+            this.txtwebclient.Name = "txtwebclient";
+            this.txtwebclient.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtwebclient.Size = new System.Drawing.Size(269, 20);
+            this.txtwebclient.TabIndex = 0;
+            // 
+            // getwebpage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 416);
+            this.Controls.Add(this.txtwebclient);
+            this.Name = "getwebpage";
+            this.Text = "getwebpage";
+            this.Load += new System.EventHandler(this.getwebpage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtwebclient;
+    }
+}
---
+++ advanced_ckey/getwebpage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace advanced_ckey
+{
+    public partial class getwebpage : Form
+    {
+        public getwebpage()
+        {
+            InitializeComponent();
+        }
+
+        private void getwebpage_Load(object sender, EventArgs e)
+        {
+
+
+            //using (var webClient = new System.Net.WebClient())
+            //{
+            //    var json = webClient.DownloadString("http://localhost/widgets/Screenshot_save?pc_widget_output_method=JSON");
+            //    Newtonsoft.Json.Linq.JObject o = Newtonsoft.Json.Linq.JObject.Parse(json);
+            //    var value = (int)o["sucess"]["File is saved at 25"];
+            //    txtwebclient.Text = value.ToString();
+            //}
+
+
+
+            //using(var webclient = new WebClient()) 
+            //{
+            //    string rawJSON = webclient.DownloadString("http://localhost/widgets/Screenshot_show?pc_widget_output_method=JSON");
+            //    json collection = JsonConvert.DeserializeObject<json>(rawJSON);
+            //}
+
+
+
+
+
+               WebClient client = new WebClient();   //gets an object to access a website
+               string download = client.DownloadString("http://localhost/widgets/Screenshot_show?pc_widget_output_method=JSON"); //download the json file into a string
+               dynamic dobj = JsonConvert.DeserializeObject<dynamic>(download); //this function of newtonsoft here deserialize json data 
+
+            string location = (string)dobj["JSON"];
+
+
+               txtwebclient.Text = location;
+
+
+        }
+    }
+}
