77899fdb15e921351af6d8e337dc6cb774c8e635
Malik Adeoye Oseni <57351965+MalikOseni@users.noreply.github.com>
2020-03-19 15:09:55 +0100

    the user authentication from malik tested and working the one time user login

---
+++ advanced_ckey/Formlog.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace advanced_ckey
+{
+    partial class Formlog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uname = new System.Windows.Forms.Label();
+            this.passw = new System.Windows.Forms.Label();
+            this.txtuname = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.url = new System.Windows.Forms.Label();
+            this.txturl = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uname
+            // 
+            this.uname.AutoSize = true;
+            this.uname.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uname.Location = new System.Drawing.Point(49, 171);
+            this.uname.Name = "uname";
+            this.uname.Size = new System.Drawing.Size(100, 24);
+            this.uname.TabIndex = 0;
+            this.uname.Text = "UserName";
+            // 
+            // passw
+            // 
+            this.passw.AutoSize = true;
+            this.passw.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passw.Location = new System.Drawing.Point(49, 214);
+            this.passw.Name = "passw";
+            this.passw.Size = new System.Drawing.Size(92, 24);
+            this.passw.TabIndex = 1;
+            this.passw.Text = "Password";
+            // 
+            // txtuname
+            // 
+            this.txtuname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtuname.Location = new System.Drawing.Point(156, 171);
+            this.txtuname.Name = "txtuname";
+            this.txtuname.Size = new System.Drawing.Size(253, 26);
+            this.txtuname.TabIndex = 2;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpass.Location = new System.Drawing.Point(156, 214);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(253, 26);
+            this.txtpass.TabIndex = 3;
+            this.txtpass.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(182, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 54);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(557, 37);
+            this.panel1.TabIndex = 5;
+            this.panel1.Visible = false;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(220, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Error";
+            // 
+            // url
+            // 
+            this.url.AutoSize = true;
+            this.url.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.url.Location = new System.Drawing.Point(49, 126);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(78, 24);
+            this.url.TabIndex = 6;
+            this.url.Text = "Web Url";
+            // 
+            // txturl
+            // 
+            this.txturl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txturl.Location = new System.Drawing.Point(156, 126);
+            this.txturl.Name = "txturl";
+            this.txturl.Size = new System.Drawing.Size(253, 26);
+            this.txturl.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(520, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 26);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(413, 220);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(79, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Show Pass";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.Location = new System.Drawing.Point(301, 263);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 54);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Formlog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 426);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txturl);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuname);
+            this.Controls.Add(this.passw);
+            this.Controls.Add(this.uname);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Formlog";
+            this.Text = "Formlog";
+            this.Load += new System.EventHandler(this.Formlog_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label uname;
+        private System.Windows.Forms.Label passw;
+        private System.Windows.Forms.TextBox txtuname;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label url;
+        private System.Windows.Forms.TextBox txturl;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ advanced_ckey/Formlog.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace advanced_ckey
+{
+    public partial class Formlog : Form
+    {
+        public Formlog()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (txturl.Text == string.Empty)
+            {
+                label1.Text = "Url Missing";
+                panel1.Visible = true;
+                txturl.Focus();
+                return;
+            }
+            if (txtuname.Text == string.Empty)
+            {
+                label1.Text = "username missing";
+                panel1.Visible = true;
+                txtuname.Focus();
+                return;
+            }
+            if (txtpass.Text == string.Empty)
+            {
+                label1.Text = "Password Missing";
+                panel1.Visible = true;
+                txtpass.Focus();
+                return;
+            }
+
+            Properties.Settings.Default.username = txtuname.Text;
+            Properties.Settings.Default.password = txtpass.Text;
+            Properties.Settings.Default.weburl = txturl.Text;
+            Properties.Settings.Default.Save();
+
+            this.Hide();
+            Form1 log = new Form1();
+            log.ShowDialog();
+
+
+        }
+
+        private void Formlog_Load(object sender, EventArgs e)
+        {
+           // Properties.Settings.Default.Reset(); 
+
+            if (Properties.Settings.Default.username != string.Empty && Properties.Settings.Default.password != string.Empty)
+            {
+
+                this.Hide();
+                Form1 log = new Form1();
+                log.ShowDialog();
+                this.Close();
+
+            }
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true && txtpass.UseSystemPasswordChar == true)
+            {
+                txtpass.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtpass.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result1 = MessageBox.Show("You Sure About Exit?", "Important Question", MessageBoxButtons.YesNo);
+            if (result1 == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            
+        }
+    }
+}
+
